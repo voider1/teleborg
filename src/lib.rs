d5ac62ee3019b9c16f62e0007295dc1585d6ec8c
voider1 <wesleyey0408@gmail.com>
2016-11-22 22:40:53 +0100

    Adding getMe command.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,45 @@
-const BASE_URL: &'static str = "https://api.telegram.org/bot";
+extern crate reqwest;
 
+use reqwest::{Response};
+
+pub const BASE_URL: &'static str = "https://api.telegram.org/bot";
+
+/// A struct which contains things associated with the bot.
 pub struct Bot {
 	token: String,
 	bot_url: String,
 }
 
 impl Bot {
-	fn new(token: String) -> Bot {
-		let bot_url = [BASE_URL, &*token].concat();
+    /// Return a new bot struct.
+    /// Panic! if something goes wrong.
+	pub fn new(token: String) -> Bot {
 
-		Bot {
+		let bot_url = [BASE_URL, &token].concat();
+		let bot = Bot {
 			token: token,
 			bot_url: bot_url,
-		}
+		};
+        let response = bot.get_me();
+
+        if response.status().is_success() {
+            bot
+        } else {
+            panic!("An error has occured.");
+        }
 	}
+
+    /// A simple test to see if the bot exists.
+	fn get_me(&self) -> Response {
+	    let path = ["getMe"];
+	    let url = construct_api_url(self, &path);
+	    reqwest::get(&url).unwrap()
+    }
 }
 
-fn construct_api_url(bot: Bot, path: &[&str]) -> String {
-    let path = path.join("/");
-    [bot.bot_url, path].join("/")
+/// Construct an API URL with the base bot URL and an
+/// array of strings which will construct the path.
+fn construct_api_url(bot: &Bot, path: &[&str]) -> String {
+
+    format!("{}/{}", bot.bot_url, path.join("/"))
 }
--- tests/test.rs
+++ tests/test.rs
@@ -2,4 +2,11 @@ extern crate teleborg;
 
 #[cfg(test)]
 mod tests {
+    use teleborg::*;
+    const BOT_ID: &'static str = "281675190:AAGG60c26-fBcfQJxU_1lTqoZyWMVPVl0EQ";
+
+    #[test]
+    fn create_bot() {
+        Bot::new(BOT_ID.to_string());
+    }
 }
