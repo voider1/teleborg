2f3949fe9959b78d0448c1d90868bf0a9609abe2
Wesley Gahr <wesleyey0408@gmail.com>
2016-11-25 10:27:46 +0100

    Updating get updates method.

--- src/updater.rs
+++ src/updater.rs
@@ -1,3 +1,7 @@
+extern crate json;
+
+use std::io::Read;
+
 use reqwest;
 
 use bot;
@@ -11,6 +15,7 @@ pub struct Updater {
 }
 
 impl Updater {
+	/// Creates a new Updater struct.
     pub fn new(token: String) -> Updater {
 		let bot_url = [BASE_URL, &token].concat();
 		let bot = bot::Bot::new(bot_url);
@@ -24,9 +29,18 @@ impl Updater {
     }
 
     /// Function to get all the messages for the bot.
-    pub fn get_updates(&self) -> reqwest::Response {
+    pub fn get_updates(&self) -> json::JsonValue {
     	let path = ["getUpdates"];
     	let url = ::construct_api_url(&self.bot.bot_url, &path);
-    	self.client.get(&url).send().unwrap()	
+    	let mut resp = self.client.get(&url).send().unwrap();
+		let mut body = String::new();
+		resp.read_to_string(&mut body).unwrap();
+
+    	if resp.status().is_success() {
+    		let rjson = json::parse(&body).unwrap();
+    		rjson["result"].clone()
+    	} else {
+    		panic!(body);
+    	}
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,22 +1,18 @@
 extern crate teleborg;
 
-use std::io::Read;
-
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    use std::io::Read;
+
     use teleborg::updater::*;
+
     const BOT_ID: &'static str = "";
 
     #[test]
     fn create_bot() {
         let updater = Updater::new(BOT_ID.to_string());
-        let resp = updater.get_updates();
-        if resp.status().is_success() {
-            let mut body = String::new();
-            resp.read_to_string(&mut body).unwrap();
-            println!("{}", &body);
-        } else {
-            panic!("ERROR!");
-        }
+        let mut resp = updater.get_updates();
     }
 }
