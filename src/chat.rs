81454953ed014a79e58a909ad88a481daae8fab0
voider1 <wesleyey0408@gmail.com>
2017-01-01 05:32:06 +0100

    Adding more structs

---
+++ src/chat.rs
@@ -0,0 +1,24 @@
+use serde_derive;
+use serde_json;
+
+use error::{Error, Result};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Chat {
+    id: i64,
+    #[serde(rename="type")]
+    type_chat: String,
+    title: Option<String>,
+    username: Option<String>,
+    first_name: Option<String>,
+    last_name: Option<String>,
+    #[serde(rename="all_members_are_administrators")]
+    all_members_are_admins: Option<bool>,
+}
+
+impl Chat {
+    pub fn new(json: &serde_json::Value) -> Result<Chat> {
+        let chat: Chat = serde_json::from_value(json.clone())?;
+        Ok(chat)
+    }
+}
