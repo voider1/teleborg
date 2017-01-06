fa29b05ca4e0c992758abf3e0b0d6791332dd187
voider1 <wesleyey0408@gmail.com>
2017-01-06 23:06:13 +0100

    Moving value extension to separate file

---
+++ src/value_extension.rs
@@ -0,0 +1,37 @@
+use serde_json;
+
+use error::{Error, Result};
+
+pub trait ValueExtension {
+    fn as_optional_string(&self, field: &str) -> Option<String>;
+
+    fn as_required_string(&self, field: &str) -> Result<String>;
+
+    fn as_optional_i64(&self, field: &str) -> Option<i64>;
+
+    fn as_required_i64(&self, field: &str) -> Result<i64>;
+
+    fn as_optional_bool(&self, field: &str) -> Option<bool>;
+}
+
+impl ValueExtension for serde_json::Value {
+    fn as_optional_string(&self, field: &str) -> Option<String> {
+        self.find(field).and_then(|v| v.as_str()).map(|s| s.to_owned())
+    }
+
+    fn as_required_string(&self, field: &str) -> Result<String> {
+        self.find(field).and_then(|v| v.as_str()).map(|s| s.to_owned()).ok_or(Error::JsonNotFound)
+    }
+
+    fn as_optional_i64(&self, field: &str) -> Option<i64> {
+        self.find(field).and_then(|v| v.as_i64())
+    }
+
+    fn as_required_i64(&self, field: &str) -> Result<i64> {
+        self.find(field).and_then(|v| v.as_i64()).ok_or(Error::JsonNotFound)
+    }
+
+    fn as_optional_bool(&self, field: &str) -> Option<bool> {
+        self.find(field).and_then(|v| v.as_bool())
+    }
+}
