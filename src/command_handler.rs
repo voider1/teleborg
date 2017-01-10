5136f0b109427338ebac34cbcb7e9d5c47f8a580
voider1 <wesleyey0408@gmail.com>
2017-01-10 09:51:05 +0100

    Removing debug print statements

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -18,11 +18,9 @@ impl CommandHandler {
 
     pub fn add<C: Command>(&mut self, command_name: &str, command: C) {
         self.commands.insert(command_name.to_string(), Box::new(command));
-        println!("Added command!");
     }
 
     pub fn start_command_handling(&mut self, rx: mpsc::Receiver<Update>, bot: &Bot) {
-        println!("called startcommandhandling!");
         loop {
             let update = rx.recv().unwrap();
 
@@ -30,7 +28,6 @@ impl CommandHandler {
                 if t.starts_with("/") {
                     let (_, bot_command) = t.split_whitespace().next().unwrap().split_at(1);
                     let command = self.commands.get_mut(bot_command);
-                    println!("{}", bot_command);
 
                     if let Some(c) = command {
                         c.execute(bot, update);
