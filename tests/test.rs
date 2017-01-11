da4f32657fb310765b1cc0e4645ad88d99cb9285
Bertus Wisman <bertuswisman@bertuss-air.home>
2017-01-11 20:57:57 +0100

    added test function for inline

--- tests/test.rs
+++ tests/test.rs
@@ -14,24 +14,38 @@ mod tests {
     fn create_updater() {
         let mut commands = command_handler::CommandHandler::new();
         commands.add("test", test);
+        commands.add("inl_test", test_inline_keyboard);
         updater::Updater::start(None, None, None, None, commands);
     }
 
     fn test(bot: &Bot, update: Update) {
+        let x = bot.send_message(chat_id,
+                                 "<p>paragraph</p>",
+                                 Some(&ParseMode::Html),
+                                 None,
+                                 None,
+                                 None, 
+                                 None);
+        println!("{:?}", x);
+
+    }
+
+    fn test_inline_keyboard(bot: &Bot, update: Update) {
         let chat_id = &update.message.unwrap().chat.id;
+        
         let mut row = Vec::<Button>::new();
-        row.push(Button::new("TestButton".to_string(), None, Some("test".to_string()), None, None));
+        row.push(Button::new("TestButton".to_string(), None, None, None, None));
         let mut total = Vec::<Vec<Button>>::new();
         total.push(row);
         let markup = Markup::new(total);
+
         let x = bot.send_message(chat_id,
                                  "GET THE NEW TELEBORG API!!!",
-                                 Some(&ParseMode::Html),
+                                 None,
                                  None,
                                  None,
                                  None, 
                                  Some(&markup));
         println!("{:?}", x);
-
     }
 }
