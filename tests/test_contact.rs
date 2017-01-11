20f8148b8f8873eab212e4883bad882b336b7f1f
Kamran Mackey <kamranm1200@gmail.com>
2017-01-11 16:22:53 -0700

    Move test_contact over to new test structure.
    Also left a debug statement for debugging purposes.
    
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- tests/test_contact.rs
+++ tests/test_contact.rs
@@ -2,20 +2,41 @@ extern crate teleborg;
 
 #[cfg(test)]
 mod tests {
+    use teleborg::bot::Bot;
     use teleborg::objects::update::Update;
     use teleborg::updater;
+    
+    use std::env;
+
+    const BASE_URL: &'static str = "https://api.telegram.org/bot";
+
+    fn setup() -> (String, i64) {
+        let token = env::var("TELEGRAM_BOT_TOKEN")
+            .ok()
+            .expect("Can't find TELEGRAM_BOT_TOKEN env variable");
+        let chat_id = env::var("TELEGRAM_CHAT_ID")
+            .ok()
+            .expect("Can't find TELEGRAM_CHAT_ID env variable")
+            .parse::<i64>()
+            .unwrap();
+        (token, chat_id)
+    }
 
     // This test tries to send a contact by sending a
     // simple phone number (+1 123-456-7890) along with
     // "Test Contact" as the first/last name for the contact.
-    fn test_contact(bot:&Bot, update: Update) {
-        let chat_id = &update.message.unwrap().chat.id;
-        let contact = bot.send_contact(chat_id,
+    #[test]
+    fn test_contact() {
+        let (token, chat_id) = setup();
+        let bot_url = [BASE_URL, &token].concat();
+        let bot = Bot::new(bot_url).unwrap();
+        let contact = bot.send_contact(&chat_id,
                                  "+1 123-456-7890",
                                  "Test",
                                  "Contact",
                                  None,
                                  None);
+        println!("{:?}", contact);
         assert!(contact.is_ok());
     }
 }
