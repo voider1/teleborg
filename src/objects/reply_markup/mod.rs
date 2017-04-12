1691c7638cd7353d8746560d9ba73d8f8219406e
voider1 <wesleyey0408@gmail.com>
2017-04-12 21:56:16 +0200

    Implementing ReplyKeyboardMarkup

--- src/objects/reply_markup/mod.rs
+++ src/objects/reply_markup/mod.rs
@@ -1,7 +1,9 @@
 pub use self::force_reply::ForceReply;
 pub use self::inline_keyboard::InlineKeyboardMarkup;
 pub use self::inline_keyboard_button::InlineKeyboardButton;
+pub use self::reply_keyboard_markup::ReplyKeyboardMarkup;
 
 mod force_reply;
 mod inline_keyboard;
 mod inline_keyboard_button;
+mod reply_keyboard_markup;
