a58e14f97b3dc7f0e6cf97df273d0c0015682018
b.wisman155 <b.wisman155@gmail.com>
2017-04-23 22:15:34 +0200

    remove Serialize from my objects, forgot sticker and document

--- src/objects/document.rs
+++ src/objects/document.rs
@@ -1,7 +1,7 @@
 use objects::PhotoSize;
 
 /// Represents a document
-#[derive(Clone, Serialize, Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct Document {
     pub file_id: String,
     pub thump: Option<PhotoSize>,
