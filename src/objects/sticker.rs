9a17c66fc261fb54140fb3eb8cc11ef2bead59f8
b.wisman155 <b.wisman155@gmail.com>
2017-04-23 21:46:40 +0200

    added Sticker object

---
+++ src/objects/sticker.rs
@@ -0,0 +1,12 @@
+use objects::PhotoSize;
+
+/// Represents a sticker
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Audio {
+    pub file_id: String,
+    pub width: i64,
+    pub heigh: i64,
+    pub thump: Option<PhotoSize>,
+    pub emoji: Option<String>,
+    pub file_size: Option<i64>,
+}
