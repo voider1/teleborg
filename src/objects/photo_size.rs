747e45c10c9ab195114e382da61a833364348075
b.wisman155 <b.wisman155@gmail.com>
2017-04-23 21:55:10 +0200

    added photo_path to PhotoSize because it is missing in telegram docs but can be present

--- src/objects/photo_size.rs
+++ src/objects/photo_size.rs
@@ -2,6 +2,7 @@
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct PhotoSize {
     pub file_id: String,
+    pub file_path: Option<String>,
     pub width: i64,
     pub height: i64,
     pub file_size: Option<i64>,
