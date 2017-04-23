b03d78456dbaa7d5f112a50db917807b21731d1a
b.wisman155 <b.wisman155@gmail.com>
2017-04-23 21:43:52 +0200

    added Audio object

---
+++ src/objects/audio.rs
@@ -0,0 +1,10 @@
+/// Represents a audio file
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Audio {
+    pub file_id: String,
+    pub duration: i64,
+    pub performer: Option<String>,
+    pub title: Option<String>,
+    pub mime_type: Option<String>,
+    pub file_size: Option<i64>,
+}
