5d24c45251aefee48c7a15c9bd8849677b947193
b.wisman155 <b.wisman155@gmail.com>
2017-04-23 21:50:28 +0200

    added Video object

---
+++ src/objects/video.rs
@@ -0,0 +1,13 @@
+use objects::PhotoSize;
+
+/// Represents a video file
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Video{
+    pub file_id: String,
+    pub width: i64,
+    pub heigh: i64,
+    pub duration: i64,
+    pub thump: Option<PhotoSize>,
+    pub mime_type: Option<String>,
+    pub file_size: Option<i64>,
+}
