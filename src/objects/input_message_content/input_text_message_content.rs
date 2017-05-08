ce176e3d54e134ae6b19d894bded498b3505d633
b.wisman155 <b.wisman155@gmail.com>
2017-05-08 21:25:10 +0200

    changed few fields to options

--- src/objects/input_message_content/input_text_message_content.rs
+++ src/objects/input_message_content/input_text_message_content.rs
@@ -1,5 +1,7 @@
+use bot::parse_mode::ParseMode;
+
 pub struct InputTextMessageContent {
     pub message_text: String,
-    pub parse_mode: ParseMode,
-    pub disable_web_page_preview: bool,
+    pub parse_mode: Option<ParseMode>,
+    pub disable_web_page_preview: Option<bool>,
 }
