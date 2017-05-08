46d5266cfe862b877fe7d2125a9d2a1801dd190f
b.wisman155 <b.wisman155@gmail.com>
2017-05-08 21:17:36 +0200

    adding input message content type `Text`

---
+++ src/objects/input_message_content/input_text_message_content.rs
@@ -0,0 +1,5 @@
+pub struct InputTextMessageContent {
+    pub message_text: String,
+    pub parse_mode: ParseMode,
+    pub disable_web_page_preview: bool,
+}
---
+++ src/objects/input_message_content/mod.rs
@@ -0,0 +1,3 @@
+pub use self::input_text_message_content::InputTextMessageContent;
+
+mod input_text_message_content;
