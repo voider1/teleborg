7fcbc1b9a54d9636d3552a246943802f220c0a7a
b.wisman155 <b.wisman155@gmail.com>
2017-05-08 21:08:34 +0200

    changed InlineQuery id to String

--- src/objects/inline_query.rs
+++ src/objects/inline_query.rs
@@ -3,7 +3,7 @@ use objects::{User, Location};
 /// Represents a Telegram inline_query.
 #[derive(Clone, Deserialize, Debug)]
 pub struct InlineQuery {
-    pub id: i64,
+    pub id: String,
     pub from: User,
     pub location: Option<Location>,
     pub query: String,
