aae51ca50349874974c3e9a573bdc7ec9aae7180
DerUberCatman <b.wisman155@gmail.com>
2017-05-08 16:27:53 +0200

    fixed typo in document.rs

--- src/objects/document.rs
+++ src/objects/document.rs
@@ -6,6 +6,6 @@ pub struct Document {
     pub file_id: String,
     pub thump: Option<PhotoSize>,
     pub file_name: Option<String>,
-    pub mime_typ: Option<String>,
+    pub mime_type: Option<String>,
     pub file_size: Option<i64>,
 }
