4ddeaed663c5099c5269187d2b8012c3deb7f3b7
DerUberCatman <b.wisman155@gmail.com>
2017-05-08 18:12:20 +0200

    changed fields to public

--- src/objects/inline_query.rs
+++ src/objects/inline_query.rs
@@ -1,11 +1,11 @@
-use objects::Location;
+use objects::{User, Location};
 
 /// Represents a Telegram inline_query.
 #[derive(Clone, Deserialize, Debug)]
 pub struct InlineQuery {
-    id: i64,
-    from: User,
-    location: Option<Location>,
-    query: String,
-    offset: String,
+    pub id: i64,
+    pub from: User,
+    pub location: Option<Location>,
+    pub query: String,
+    pub offset: String,
 }
