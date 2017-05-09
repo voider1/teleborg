d56874ad749fc9aa25f13011b0f5e34a63fae95d
DerUberCatman <b.wisman155@gmail.com>
2017-05-09 10:31:45 +0200

    added input venue message content to mod.rs

--- src/objects/input_message_content/mod.rs
+++ src/objects/input_message_content/mod.rs
@@ -1,5 +1,7 @@
 pub use self::input_text_message_content::InputTextMessageContent;
 pub use self::input_location_message_content::InputLocationMessageContent;
+pub use self::input_venue_message_content::InputVenueMessageContent;
 
 mod input_text_message_content;
 mod input_location_message_content;
+mod input_venue_message_content;
