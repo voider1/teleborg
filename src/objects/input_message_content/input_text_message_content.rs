8da234c9ec344a6e45bdb8d0946fbd94ff470e1f
DerUberCatman <b.wisman155@gmail.com>
2017-05-09 13:43:32 +0200

    created input text message content constructor

--- src/objects/input_message_content/input_text_message_content.rs
+++ src/objects/input_message_content/input_text_message_content.rs
@@ -1,10 +1,21 @@
 use objects::input_message_content::marker::InputMessageContent;
+use bot::parse_mode::{ParseMode, get_parse_mode};
 
 #[derive(Deserialize, Serialize)]
 pub struct InputTextMessageContent {
     pub message_text: String,
     pub parse_mode: Option<String>,
     pub disable_web_page_preview: Option<bool>,
+}
+
+impl InputTextMessageContent {
+    pub fn new(message_text: String, parse_mode: Option<ParseMode>, disable_web_page_preview: Option<bool>) -> Self {
+        InputTextMessageContent {
+            message_text: message_text,
+            parse_mode: Some(get_parse_mode(&parse_mode.unwrap_or(ParseMode::Text))),
+            disable_web_page_preview: disable_web_page_preview,
+        }
+    }
 }
 
 impl InputMessageContent for InputTextMessageContent {}
