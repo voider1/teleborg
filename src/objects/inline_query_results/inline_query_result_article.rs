79bf11e208e491398a35785765731e7e035807d6
DerUberCatman <b.wisman155@gmail.com>
2017-05-09 13:44:00 +0200

    implemented constructor for result article

--- src/objects/inline_query_results/inline_query_result_article.rs
+++ src/objects/inline_query_results/inline_query_result_article.rs
@@ -1,3 +1,9 @@
+extern crate rand;
+
+use std::time::Instant;
+
+use self::rand::Rng;
+
 use objects::input_message_content::InputMessageContent;
 use objects::inline_query_results::InlineQueryResult;
 
@@ -10,15 +16,21 @@ pub struct InlineQueryResultArticle <T: InputMessageContent> {
     pub input_message_content: T,
 }
 
-impl InlineQueryResultArticle<T> where T: InputMessageContent  {
+impl <T:InputMessageContent> InlineQueryResultArticle <T> {
     pub fn new(title: String, input_message_content: T) -> Self {
+        let now = Instant::now();
+        let seconds = now.elapsed().as_secs();
+
+        let mut rng = rand::thread_rng();
+        let num = rng.gen_range::<i64>(1000, 9999);
+
         InlineQueryResultArticle {
-            result_type: "article",
-            id: "",
+            result_type: "article".to_owned(),
+            id: format!("{}{}", num, seconds),
             title: title,
             input_message_content: input_message_content,
         }
     }
 }
 
-impl <T> InlineQueryResult for InlineQueryResultArticle<T> where T: InputMessageContent {}
+impl <T: InputMessageContent> InlineQueryResult for InlineQueryResultArticle<T> {}
