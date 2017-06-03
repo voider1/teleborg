f64a3db5d5ac206d9677139e123b8990e31d16cf
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 16:15:38 +0200

    changed test_updater to fit multiple types of inline query results

--- tests/test_updater.rs
+++ tests/test_updater.rs
@@ -4,8 +4,8 @@ extern crate teleborg;
 mod tests {
     use teleborg::{Dispatcher, Updater, Bot};
     use teleborg::objects::Update;
-    use teleborg::objects::inline_query_results::InlineQueryResultArticle;
-    use teleborg::objects::input_message_content::InputTextMessageContent;
+    use teleborg::objects::inline_query_results::{InlineQueryResultArticle, InlineQueryResult};
+    use teleborg::objects::input_message_content::{InputTextMessageContent, InputLocationMessageContent};
     use teleborg::{ParseMode};
 
     #[test]
@@ -22,22 +22,38 @@ mod tests {
 
     fn test_inline_query(bot: &Bot, update: Update, _: Option<Vec<&str>>) {
         if let Some(inline_query) = update.clone().inline_query {
-            let mut results = Vec::new();
-            let content = InputTextMessageContent::new("HITLER RULES BITCH".to_owned(),
+            let mut results: Vec<Box<InlineQueryResult>> = Vec::new();
+            let content = InputTextMessageContent::new("Telegram rules".to_owned(),
                                                        Some(ParseMode::Text),
                                                        Some(true));
-            let article = InlineQueryResultArticle::new("hitler rules".to_string(),
-                                                        content,
+            let article = InlineQueryResultArticle::new("Telegram text?".to_string(),
+                                                        Box::new(content),
                                                         None,
                                                         None,
                                                         None,
                                                         None,
-                                                        None,
+                                                        Some("https://telegram.org/img/t_logo.png".to_string()),
                                                         None,
                                                         None);
+
+            let content2 = InputLocationMessageContent::new(35.0, 38.0);
+            let article2 = InlineQueryResultArticle::new("Telegram location?".to_string(),
+                                                         Box::new(content2),
+                                                         None,
+                                                         None,
+                                                         None,
+                                                         None,
+                                                         Some("https://telegram.org/img/t_logo.png".to_string()),
+                                                         None,
+                                                         None);
+
+            let article = Box::new(article);
+            let article2 = Box::new(article2);
             results.push(article);
+            results.push(article2);
+
             let answer_result = bot.answer_inline_query(&update, results);
-            assert!(answer_result.is_ok());
+            println!("{:?}", answer_result.err());
         }
     }
 }
