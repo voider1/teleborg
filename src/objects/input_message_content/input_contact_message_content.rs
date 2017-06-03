ed79d20f6ff2a61fbe4264f5f3fd81266508e8e3
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 16:36:19 +0200

    adapted input_contact_message_content to fit the new trait

--- src/objects/input_message_content/input_contact_message_content.rs
+++ src/objects/input_message_content/input_contact_message_content.rs
@@ -1,10 +1,30 @@
-use objects::input_message_content::marker::InputMessageContent;
+use super::{InputMessageContent, InputMessageType};
 
-#[derive(Serialize, Deserialize, Debug)]
+use std::any::Any;
+
+#[derive(Serialize, Deserialize)]
 pub struct InputContactMessageContent {
     pub phone_number: String,
     pub first_name: String,
     pub last_name: Option<String>,
 }
 
-impl InputMessageContent for InputContactMessageContent {}
+impl InputContactMessageContent {
+    pub fn new(phone_number: String, first_name: String, last_name: Option<String>) -> Self {
+        InputContactMessageContent{
+            phone_number: phone_number,
+            first_name: first_name,
+            last_name: last_name,
+        }
+    }
+}
+
+impl InputMessageContent for InputContactMessageContent {
+    fn as_any(&self) -> &Any {
+        self
+    }
+
+    fn get_type(&self) -> InputMessageType {
+        InputMessageType::Contact
+    }
+}
