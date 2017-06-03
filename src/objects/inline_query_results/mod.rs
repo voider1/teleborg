383909669081c2a068a3375e942c02d8fce0c32c
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 16:20:35 +0200

    added Serialize implement for boxed InlineQueryResult trait

--- src/objects/inline_query_results/mod.rs
+++ src/objects/inline_query_results/mod.rs
@@ -1,5 +1,18 @@
 pub use self::inline_query_result_article::InlineQueryResultArticle;
-pub use self::marker::InlineQueryResult;
+pub use self::marker::{InlineQueryResult};
+pub use self::inline_query_result_type::InlineQueryResultType;
 
 mod inline_query_result_article;
 mod marker;
+mod inline_query_result_type;
+
+use serde::{Serialize, Serializer};
+
+impl Serialize for Box<InlineQueryResult> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        match self.get_type() {
+            InlineQueryResultType::Article =>
+                serializer.serialize_some((&**self).as_any().downcast_ref::<InlineQueryResultArticle>().unwrap())
+        }
+    }
+}
