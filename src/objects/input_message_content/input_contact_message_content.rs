3d4380eda70f68c12748771af9e4d343cde16dbf
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 17:06:09 +0200

    added serde skip for optional fields

--- src/objects/input_message_content/input_contact_message_content.rs
+++ src/objects/input_message_content/input_contact_message_content.rs
@@ -6,6 +6,7 @@ use std::any::Any;
 pub struct InputContactMessageContent {
     pub phone_number: String,
     pub first_name: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub last_name: Option<String>,
 }
 
--- src/objects/input_message_content/input_venue_message_content.rs
+++ src/objects/input_message_content/input_venue_message_content.rs
@@ -8,6 +8,7 @@ pub struct InputVenueMessageContent {
     pub longitude: f64,
     pub title: String,
     pub address: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub foursquare_id: Option<String>,
 }
 
