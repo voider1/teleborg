afae70744fd2c73234764e1045c4820285d28a08
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 17:06:41 +0200

    added test for contact and venue

--- tests/test_updater.rs
+++ tests/test_updater.rs
@@ -5,7 +5,10 @@ mod tests {
     use teleborg::{Dispatcher, Updater, Bot};
     use teleborg::objects::Update;
     use teleborg::objects::inline_query_results::{InlineQueryResultArticle, InlineQueryResult};
-    use teleborg::objects::input_message_content::{InputTextMessageContent, InputLocationMessageContent};
+    use teleborg::objects::input_message_content::{InputTextMessageContent,
+                                                   InputLocationMessageContent,
+                                                   InputContactMessageContent,
+                                                   InputVenueMessageContent};
     use teleborg::{ParseMode};
 
     #[test]
@@ -23,11 +26,11 @@ mod tests {
     fn test_inline_query(bot: &Bot, update: Update, _: Option<Vec<&str>>) {
         if let Some(inline_query) = update.clone().inline_query {
             let mut results: Vec<Box<InlineQueryResult>> = Vec::new();
-            let content = InputTextMessageContent::new("Telegram rules".to_owned(),
+            let content_text = InputTextMessageContent::new("Telegram rules".to_owned(),
                                                        Some(ParseMode::Text),
                                                        Some(true));
-            let article = InlineQueryResultArticle::new("Telegram text?".to_string(),
-                                                        Box::new(content),
+            let article_text = InlineQueryResultArticle::new("Telegram text?".to_string(),
+                                                        Box::new(content_text),
                                                         None,
                                                         None,
                                                         None,
@@ -36,9 +39,9 @@ mod tests {
                                                         None,
                                                         None);
 
-            let content2 = InputLocationMessageContent::new(35.0, 38.0);
-            let article2 = InlineQueryResultArticle::new("Telegram location?".to_string(),
-                                                         Box::new(content2),
+            let content_location = InputLocationMessageContent::new(35.0, 38.0);
+            let article_location = InlineQueryResultArticle::new("Telegram location?".to_string(),
+                                                         Box::new(content_location),
                                                          None,
                                                          None,
                                                          None,
@@ -46,11 +49,37 @@ mod tests {
                                                          Some("https://telegram.org/img/t_logo.png".to_string()),
                                                          None,
                                                          None);
+            let content_contact = InputContactMessageContent::new(String::from("06-12345678"),
+                                                                  String::from("Voider1"),
+                                                                  None);
+            let article_contact = InlineQueryResultArticle::new("Telegram contact?".to_string(),
+                                                                Box::new(content_contact),
+                                                                None,
+                                                                None,
+                                                                None,
+                                                                None,
+                                                                Some("https://telegram.org/img/t_logo.png".to_string()),
+                                                                None,
+                                                                None);
+            let content_venue = InputVenueMessageContent::new(35.0,
+                                                              38.0,
+                                                              "test title".to_string(),
+                                                              "test address".to_string(),
+                                                              None);
+            let article_venue = InlineQueryResultArticle::new("Telegram venue?".to_string(),
+                                                              Box::new(content_venue),
+                                                              None,
+                                                              None,
+                                                              None,
+                                                              None,
+                                                              Some("https://telegram.org/img/t_logo.png".to_string()),
+                                                              None,
+                                                              None);
 
-            let article = Box::new(article);
-            let article2 = Box::new(article2);
-            results.push(article);
-            results.push(article2);
+            results.push(Box::new(article_text));
+            results.push(Box::new(article_location));
+            results.push(Box::new(article_contact));
+            results.push(Box::new(article_venue));
 
             let answer_result = bot.answer_inline_query(&update, results);
             println!("{:?}", answer_result.err());
