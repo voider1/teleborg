dd0ad3451394090d306a266ab8443ac4637de66f
DerUberCatman <b.wisman155@gmail.com>
2017-06-03 16:19:54 +0200

    changed InlineQueryResultArticle to use trait objects instead of generics. added serde skips for nullable fields

--- src/objects/inline_query_results/inline_query_result_article.rs
+++ src/objects/inline_query_results/inline_query_result_article.rs
@@ -4,35 +4,45 @@ use objects::input_message_content::InputMessageContent;
 use objects::inline_query_results::InlineQueryResult;
 use objects::inline_keyboard::InlineKeyboardMarkup;
 
-#[derive(Serialize, Debug)]
-pub struct InlineQueryResultArticle<I> {
+use super::InlineQueryResultType;
+
+use std::any::Any;
+
+#[derive(Serialize)]
+pub struct InlineQueryResultArticle {
     #[serde(rename="type")]
     pub result_type: String,
     pub id: String,
     pub title: String,
-    pub input_message_content: Box<I>,
+    pub input_message_content: Box<InputMessageContent>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub reply_markup: Option<InlineKeyboardMarkup>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub hide_url: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub description: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub thumb_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub thumb_width: Option<i64>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub thumb_height: Option<i64>,
 }
 
-impl <I: InputMessageContent>InlineQueryResultArticle<I> {
+impl InlineQueryResultArticle{
     pub fn new(title: String,
-               input_message_content: I,
+               input_message_content: Box<InputMessageContent>,
                reply_markup: Option<InlineKeyboardMarkup>,
                url: Option<String>,
                hide_url: Option<bool>,
                description: Option<String>,
                thumb_url: Option<String>,
                thumb_width: Option<i64>,
-               thumb_height: Option<i64>) -> InlineQueryResultArticle<I> {
+               thumb_height: Option<i64>) -> InlineQueryResultArticle {
         let result_type = "article".to_string();
         let uuid = format!("{}", Uuid::new_v4());
-        let input_message_content = Box::new(input_message_content);
 
         InlineQueryResultArticle {
             result_type: result_type,
@@ -50,4 +60,12 @@ impl <I: InputMessageContent>InlineQueryResultArticle<I> {
     }
 }
 
-impl <I: InputMessageContent>InlineQueryResult for InlineQueryResultArticle<I> {}
+impl InlineQueryResult for InlineQueryResultArticle {
+    fn as_any(&self) -> &Any {
+        self
+    }
+
+    fn get_type(&self) -> InlineQueryResultType {
+        InlineQueryResultType::Article
+    }
+}
