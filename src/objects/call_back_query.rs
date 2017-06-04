732b7eedc73f4a4e0677608b22941894380c2df1
DerUberCatman <b.wisman155@gmail.com>
2017-06-04 15:28:38 +0200

    updated call_back_query docs

--- src/objects/call_back_query.rs
+++ src/objects/call_back_query.rs
@@ -1,7 +1,12 @@
 use objects::message::Message;
 use objects::user::User;
 
-/// Represents an incoming callback query from a `InlineKeyboardButton` in an `InlineKeyboardMarkup`.
+/// Represents an incoming callback query from a callback button in an inline keyboard.
+/// If the button that originated the query was attached to a message sent by the bot,
+/// the field message will be present.
+/// If the button was attached to a message sent via the bot (in inline mode),
+/// the field inline_message_id will be present.
+/// Exactly one of the fields data or game_short_name will be present.
 #[derive(Clone, Deserialize, Debug)]
 pub struct CallBackQuery {
     pub id: String,
