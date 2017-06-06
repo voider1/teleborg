8457ea88825728342843d8342d0eb7ecd6f22195
DerUberCatman <b.wisman155@gmail.com>
2017-06-06 17:26:26 +0200

    changed message_id to String

--- src/objects/chosen_inline_result.rs
+++ src/objects/chosen_inline_result.rs
@@ -2,10 +2,11 @@ use objects::User;
 use objects::Location;
 
 /// Represents a result of an inline query that was chosen by the user and sent to their chat partner.
+#[derive(Clone, Deserialize, Debug)]
 pub struct ChosenInlineResult {
     pub result_id: String,
     pub from: User,
     pub location: Option<Location>,
-    pub inline_message_id: Option<i64>,
+    pub inline_message_id: Option<String>,
     pub query: String,
 }
