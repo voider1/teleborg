9ea39539b431143cf6dda72ab2009813214dccd5
DerUberCatman <b.wisman155@gmail.com>
2019-04-18 23:33:32 +0200

    added edit_message_live_location

---
+++ src/methods/edit_message_live_location.rs
@@ -0,0 +1,34 @@
+use super::Method;
+
+use serde::Serialize;
+use typed_builder::TypedBuilder;
+
+use crate::types::{Message, ReplyMarkup};
+
+/// Use this method to point on the map. On success, the sent `Message` is returned.
+#[derive(Debug, TypedBuilder, Serialize)]
+pub struct EditMessageLiveLocation {
+    /// Required if inline_message_id is not specified.
+    ///	Unique identifier for the target chat or username of the target channel
+    ///	(in the format @channelusername)
+    #[builder(default)]
+    pub chat_id: Option<i64>,
+    /// Required if inline_message_id is not specified. Identifier of the message to edit
+    #[builder(default)]
+    pub message_id: Option<i32>,
+    ///	Required if chat_id and message_id are not specified. Identifier of the inline message
+    #[builder(default)]
+    pub inline_message_id: Option<i64>,
+    /// Latitude of the location.
+    #[builder(default)]
+    pub latitude: f32,
+    /// Longitude of the location.
+    #[builder(default)]
+    pub longitude: f32,
+    #[builder(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    /// Interface options.
+    pub reply_markup: Option<ReplyMarkup>,
+}
+
+impl_method!(EditMessageLiveLocation, Message, "editMessageLiveLocation");
