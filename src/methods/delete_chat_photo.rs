84f8ad983009ecccd75d3e06fc8f2eef85a23e4a
DerUberCatman <b.wisman155@gmail.com>
2019-04-20 23:41:58 +0200

    updated delete_chat_photo

--- src/methods/delete_chat_photo.rs
+++ src/methods/delete_chat_photo.rs
@@ -1,5 +1,10 @@
 use super::Method;
+use serde::Serialize;
+use typed_builder::TypedBuilder;
 
+/// Use this method to delete a chat photo. Photos can't be changed for private chats.
+/// The bot must be an administrator in the chat for this to work and must have the appropriate admin rights.
+/// Returns True on success.
 #[derive(Debug, TypedBuilder, Serialize)]
 pub struct DeleteChatPhoto {
     /// Unique identifier for the target chat.
