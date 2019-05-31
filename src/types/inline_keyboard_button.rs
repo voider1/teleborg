4cf3b5d7724386fee0929a43bfee73a4bbef8ae1
Wesley Gahr <wesley.gahr@me.com>
2019-05-31 17:31:06 +0200

    Implementing TypedBuilder for InlineKeyboardButton

--- src/types/inline_keyboard_button.rs
+++ src/types/inline_keyboard_button.rs
@@ -1,32 +1,12 @@
 use serde::Serialize;
+use typed_builder::TypedBuilder;
 
 /// Represents one button of an `InlineKeyboardMarkup`, you must use exactly one of the optional fields.
-#[derive(Debug, Default, Clone, Serialize)]
+#[derive(Debug, Default, Clone, TypedBuilder, Serialize)]
 pub struct InlineKeyboardButton {
     text: String,
     url: Option<String>,
     callback_data: Option<String>,
     switch_inline_query: Option<String>,
     switch_inline_query_current_chat: Option<String>,
-}
-
-impl InlineKeyboardButton {
-    /// Constructs a new `InlineKeyboardButton`.
-    pub fn new(
-        text: String,
-        url: Option<String>,
-        callback_data: Option<String>,
-        switch_inline_query: Option<String>,
-        switch_inline_query_current_chat: Option<String>,
-    ) -> InlineKeyboardButton {
-        InlineKeyboardButton {
-            text,
-            url: Some(url.unwrap_or_else(|| "".to_string())),
-            callback_data: Some(callback_data.unwrap_or_else(|| "".to_string())),
-            switch_inline_query: Some(switch_inline_query.unwrap_or_else(|| "".to_string())),
-            switch_inline_query_current_chat: Some(
-                switch_inline_query_current_chat.unwrap_or_else(|| "".to_string()),
-            ),
-        }
-    }
 }
